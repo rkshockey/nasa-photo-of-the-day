9d897f2c4fc48ba64965dad871e3836acd3def0c
rkshockey <rkshockey@gmail.com>
2021-07-14 17:11:39 -0400

    Added effects and began on Year tab

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import { base_url, api_key } from './constants'
-
-//Setting up git!
+import calendarD from './datesDummy'
+import converter from './dateConverter'
+import YearTab from './Components/year'
 
 function App() {
-  axios.get(`${base_url}?api_key=${api_key}`)
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
+  const [year, setYear] = useState(`2021`);
+  const [month, setMonth] = useState(`07`);
+  const [date, setDate] = useState(`2021-07-14`);
+  const [nasaData, setNasaData] = useState([]);
+  const [calendar, setCalendar] = useState(calendarD);
+  const [currentCal, setCurrentCal] = useState(calendar[2021][`Jul`]);
+
+  useEffect(() => {
+    axios.get(`${base_url}?start_date=${year}-${month}-01&end_date=${year}-${month}-${currentCal}&api_key=${api_key}`)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }, [currentCal])
+
+  useEffect(() => {
+    const monthB = converter(month)
+    setCurrentCal(calendar[year][monthB])
+  }, [year, month])
 
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="container">
+      <h1>NASA Astronomy Picture of the Day</h1>
+      <YearTab setYear={setYear} calendar={calendar} />
     </div>
   );
 }
---
+++ src/Components/year.js
@@ -0,0 +1,9 @@
+import React from "react";;
+
+function YearTab(props){
+    const { setYear, calendar } = props
+
+    return null
+}
+
+export default YearTab
