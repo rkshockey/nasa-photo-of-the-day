872fc149a8170f4c843da7492104bb67bbffa52d
rkshockey <rkshockey@gmail.com>
2021-07-14 17:29:37 -0400

    Implemented year tab

--- src/Components/year.js
+++ src/Components/year.js
@@ -3,7 +3,25 @@ import React from "react";;
 function YearTab(props){
     const { setYear, calendar } = props
 
-    return null
+    function click (year) {
+        setYear(year)
+    }
+
+    function iterate (obj) {
+        const buttons = []
+        for (const property in obj) {
+            buttons.push(property)
+        }
+        return buttons
+    }
+
+    const years = iterate(calendar)
+
+    return (
+        <div className='years'>
+            {years.map(year => <button onClick={() => click(year)}>{year}</button>)}
+        </div>
+    )
 }
 
 export default YearTab
