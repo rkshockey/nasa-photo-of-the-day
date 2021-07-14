c9d9eca22d461670344e5b6f4615217b9169f10a
rkshockey <rkshockey@gmail.com>
2021-07-14 14:47:25 -0400

    Important today's data from NASA API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios';
+import { base_url, api_key } from './constants'
 
 //Setting up git!
 
 function App() {
+  axios.get(`${base_url}?api_key=${api_key}`)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+
   return (
     <div className="App">
       <p>
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const base_url = `https://api.nasa.gov/planetary/apod`
+export const api_key = `tEPCdNy6V2SUJuMCoP3iVRXqhecMLujth8cH5YFN`
